fcfbfe90c20f69663ac5ee13efeb3ee3014ca345
niklas <nyholmniklas@gmail.com>
2017-10-24 21:46:13 +0300

    Initial files

---
+++ src/App.elm
@@ -0,0 +1,31 @@
+module App exposing (..)
+
+import Html exposing (Html, div, text)
+
+update: Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  (model, Cmd.none)
+
+type alias Model = {
+  greeting: String
+}
+
+model: Model
+model =
+  { greeting = "Let's get started!" }
+
+type Msg = NoOp
+
+view : Model -> Html Msg
+view model =
+  div [] [
+    text model.greeting
+  ]
+
+main =
+  Html.program
+    { init = (model, Cmd.none)
+    , view = view
+    , update = update
+    , subscriptions = always Sub.none
+    }
